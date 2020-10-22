1b72d830543ab4622bdfdc61a63c0d58a781f091
Harrison Chikwe <nhcdeveloper@gmail.com>
2020-10-22 15:22:06 +0100

    Add src/chunk.h src/chunk.c src/memory.h src/memory.c

---
+++ src/chunk.h
@@ -0,0 +1,23 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "common.h"
+
+typedef uint8_t Byte;
+
+typedef enum
+{
+    OP_RETURN,
+} OpCode;
+
+typedef struct {
+    int count;
+    int capacity;
+    Byte *code;
+} Chunk;
+
+void initChunk(Chunk *chunk);
+void freeChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, Byte byte);
+
+#endif
