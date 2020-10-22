ab8bc45d7c79da0d221a8594aac39b3cad9f9db4
Harrison Chikwe <nhcdeveloper@gmail.com>
2020-10-22 23:54:57 +0100

    Implement interpret and run method in vm.c

--- src/vm.c
+++ src/vm.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 #include "common.h"
 #include "vm.h"
 
@@ -5,8 +7,45 @@ VM vm;
 
 void initVM()
 {
+    vm.chunk = NULL;
+    vm.ip = NULL;
 }
 
 void freeVM()
 {
+}
+
+static InterpretResult run()
+{
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+    for (;;)
+    {
+        Byte instruction;
+        switch (instruction = READ_BYTE())
+        {
+        case OP_CONSTANT:
+        {
+            Value constant = READ_CONSTANT();
+            printValue(constant);
+            printf("\n");
+            break;
+        }
+        case OP_RETURN:
+        {
+            return INTERPRET_OK;
+        }
+        }
+    }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
+
+InterpretResult interpret(Chunk *chunk)
+{
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
 }
--- src/vm.h
+++ src/vm.h
@@ -2,13 +2,23 @@
 #define clox_vm_h
 
 #include "chunk.h"
+#include "common.h"
 
-typedef struct 
+typedef struct
 {
     Chunk *chunk;
+    Byte *ip;
 } VM;
+
+typedef enum
+{
+    INTERPRET_OK,
+    INTERPRET_COMPILE_ERROR,
+    INTERPRET_RUNTIME_ERROR
+} InterpretResult;
 
 void initVM();
 void freeVM();
+InterpretResult interpret(Chunk *chunk);
 
 #endif
