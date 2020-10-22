10d92a682e04c8fa64a4386ebae2fb58214a474e
Harrison Chikwe <nhcdeveloper@gmail.com>
2020-10-22 21:32:06 +0100

    Update disassembleInstruction

--- src/debug.c
+++ src/debug.c
@@ -34,7 +34,7 @@ int disassembleInstruction(Chunk *chunk, int offset)
     printf("%04d ", offset);
     if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1])
     {
-        printf("  | ");
+        printf("   | ");
     }
     else
     {
