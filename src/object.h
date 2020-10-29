848756deaa01572b278a0f19f355dc7933ab2ada
Harrison Chikwe <nhcdeveloper@gmail.com>
2020-10-29 13:25:10 +0100

    Bugfix: in definition of macro "AS_CSTRING"

--- src/object.h
+++ src/object.h
@@ -9,7 +9,7 @@
 #define IS_STRING(value)       isObjType(value, OBJ_STRING)
 
 #define AS_STRING(value)       ((ObjString*)AS_OBJ(value))
-#define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value)->chars))
+#define AS_CSTRING(value)      (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum
 {
