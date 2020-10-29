46092284b6324ed6c25f7f5775d102da11fe205c
Harrison Chikwe <nhcdeveloper@gmail.com>
2020-10-29 14:13:20 +0100

    Add support for tracking allocated objects and freeing them when the vm terminates

--- src/vm.h
+++ src/vm.h
@@ -13,6 +13,7 @@ typedef struct
     Byte *ip;
     Value stack[STACK_MAX];
     Value *stackTop;
+    Obj *objects;
 } VM;
 
 typedef enum
@@ -21,6 +22,8 @@ typedef enum
     INTERPRET_COMPILE_ERROR,
     INTERPRET_RUNTIME_ERROR
 } InterpretResult;
+
+extern VM vm;
 
 void initVM();
 void freeVM();
