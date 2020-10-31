8e214eba298e67a1506111b9cb3c9d418dde243e
Harrison Chikwe <nhcdeveloper@gmail.com>
2020-10-31 11:10:37 +0100

    Implement "print" statement

--- src/compiler.c
+++ src/compiler.c
@@ -114,6 +114,20 @@ static void consume(TokenType type, const char *message)
     errorAtCurrent(message);
 }
 
+static bool check(TokenType type)
+{
+    return parser.current.type == type;
+}
+
+static bool match(TokenType type)
+{
+    if (!check(type))
+        return false;
+
+    advance();
+    return true;
+}
+
 static void emitByte(Byte byte)
 {
     writeChunk(currentChunk(), byte, parser.previous.line);
@@ -159,6 +173,8 @@ static void endCompiler()
 }
 
 static void expression();
+static void statement();
+static void declaration();
 static ParseRule *getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 
@@ -339,6 +355,26 @@ static void expression()
     parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void printStatement()
+{
+    expression();
+    consume(TOKEN_SEMICOLON, "Expect ';' after value.");
+    emitByte(OP_PRINT);
+}
+
+static void declaration()
+{
+    statement();
+}
+
+static void statement()
+{
+    if (match(TOKEN_PRINT))
+    {
+        printStatement();
+    }
+}
+
 bool compile(const char *source, Chunk *chunk)
 {
     initScanner(source);
@@ -348,8 +384,12 @@ bool compile(const char *source, Chunk *chunk)
     parser.panicMode = false;
 
     advance();
-    expression();
-    consume(TOKEN_EOF, "Expect end of expression.");
+
+    while (!match(TOKEN_EOF))
+    {
+        declaration();
+    }
+
     endCompiler();
     return !parser.hadError;
 }
