a4105f8ec6af1a4dbcf7d638f42768ad944667af
Harrison Chikwe <nhcdeveloper@gmail.com>
2020-11-01 12:36:08 +0100

    Implement local variables

--- src/vm.c
+++ src/vm.c
@@ -141,6 +141,18 @@ static InterpretResult run()
         case OP_POP:
             pop();
             break;
+        case OP_GET_LOCAL:
+        {
+            Byte slot = READ_BYTE();
+            push(vm.stack[slot]);
+            break;
+        }
+        case OP_SET_LOCAL:
+        {
+            Byte slot = READ_BYTE();
+            vm.stack[slot] = peek(0);
+            break;
+        }
         case OP_GET_GLOBAL:
         {
             ObjString *name = READ_STRING();
